3a03c84a4b8ad3e0c8d7e9babdf0824420b948ac
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-03-23 23:34:34 +0300

    added date

--- form.js
+++ form.js
@@ -1,30 +1,10 @@
-<form name="myForm" action="" method="GET">
-  Enter something in the box: <br>
-  <input type="text" name="inputbox" value="">
-  <input type="button" name="button" value="Click" onClick="testResults(this.form)">
-</form>
-
-function testResults (form) {
-    var inputValue = form.inputbox.value;
-    alert ("You typed: " + inputValue);
-}
+let dateToday=new Date();
+ let month =String(dateToday.getMonth()+1)
+.padStart(2,'0');
+let day =String(dateToday.getDate())
+.padStart(2,'0');
+let year =dateToday.getFullYear();
+let output=day+ '/'+month + '/'+year +'/';
 
 
-
-
-let mybutton = document.getElementById("myBtn");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+console.log(output);
